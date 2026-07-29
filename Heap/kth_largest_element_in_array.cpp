bd94c5165f949c3aa611eb8651810f8fa9245071
mmorteo <manuelmort@gmail.com>
2026-07-29 00:55:30 -0400

    adding LC #215

---
+++ Heap/kth_largest_element_in_array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+// LC #215
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> maxHeap;
+        
+        for(int i = 0; i < nums.size();i++){
+            maxHeap.push(nums[i]);
+        }
+        for(int i = 0; i < k-1; i++){
+            maxHeap.pop();
+        }        
+        
+        
+        return maxHeap.top();
+    }
+};
